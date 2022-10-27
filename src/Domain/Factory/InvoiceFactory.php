1ffc6ec01e9cf52867a526a4bb932334513baec3
Si Thu San <sithusan@aceplussolutions.com>
2022-10-27 23:29:55 +0630

    Set Up Tests

---
+++ src/Domain/Factory/InvoiceFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BillingSystem\Invoicer\Domain\Factory;
+
+use BillingSystem\Invoicer\Domain\Entity\Order;
+
+/**
+ * @author Si Thu San
+ */
+class InvoiceFactory
+{
+    public function createFromOrder(Order $order)
+    {
+    }
+
+    public function test()
+    {
+    }
+}
---
+++ src/specs/domain/services/invoice-factory.spec.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use BillingSystem\Invoicer\Domain\Entity\Order;
+use BillingSystem\Invoicer\Domain\Factory\InvoiceFactory;
+
+/**
+ * @author Si Thu San
+ */
+
+describe('InvoiceFactory', function () {
+    fcontext('Invoice', function () {
+        describe('->createFromOrder()', function () {
+            it('should return order object', function () {
+                $order = new Order();
+                $factory = new InvoiceFactory();
+                $invoice = $factory->createFromOrder($order);
+
+                except($invoice)->to->be->instanceof(
+                    'BillingSystem\Invoicer\Domain\Entity\Invoice'
+                );
+            });
+        });
+    });
+});
